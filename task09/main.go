d9880f956d738d5ec9b08570cdf269b4f0e147b7
vanamelnik <jmvmiller@gmail.com>
2022-07-06 01:56:44 +0300

    Finished task09

--- task09/main.go
+++ task09/main.go
@@ -1,5 +1,43 @@
 package main
 
-// Задание 9
-// Разработать конвейер чисел. Даны два канала: в первый пишутся числа (x) из массива, во второй — результат операции x*2, после чего данные из второго канала должны выводиться в stdout.
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
+// Задание 9
+// Разработать конвейер чисел. Даны два канала: в первый пишутся числа (x) из массива,
+// во второй — результат операции x*2, после чего данные из второго канала должны выводиться в stdout.
+
+// producer извлекает числа из переданного массива и посылает их в канал.
+func producer(arr []int, ch chan<- int) {
+	for _, x := range arr {
+		ch <- x
+	}
+	close(ch) // закрытие канала - сигнал к окончанию работы следующего звена
+}
+
+// multiplier читает числа из канала chIn, умножает их на 2 и передаёт в канал chOut.
+func multiplier(chIn <-chan int, chOut chan<- int) {
+	for x := range chIn {
+		chOut <- x * 2
+	}
+	close(chOut)
+}
+
+func main() {
+	const arraySize = 1000
+	arr := make([]int, arraySize)
+	for i := range arr {
+		arr[i] = rand.Intn(math.MaxInt / 2) // чтобы избежать переполнения при удвоении (и появления отрицательных чисел)
+	}
+	fmt.Println()
+	prodToMult := make(chan int)
+	multToOut := make(chan int)
+	go producer(arr, prodToMult)
+	go multiplier(prodToMult, multToOut)
+	for x := range multToOut {
+		fmt.Println(x)
+	}
+}
