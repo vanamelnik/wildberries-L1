876023a3686f5cdd5b0a4c7774d4886ef4875101
vanamelnik <jmvmiller@gmail.com>
2022-07-06 13:15:28 +0300

    Completed task19

--- task19/main.go
+++ task19/main.go
@@ -3,3 +3,18 @@ package main
 // Задание 19
 // Разработать программу, которая переворачивает подаваемую на ход строку (например: «главрыба — абырвалг»). Символы могут быть unicode.
 
+import "fmt"
+
+// reverse переворачивает переданную строку.
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	s := "\xF0\x9F\x98\x8B привет 世界 \xF0\x9F\x92\xA5"
+	fmt.Printf("%q ----> %q\n", s, reverse(s))
+}
