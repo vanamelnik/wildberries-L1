3507395ea9ba6df3a4dab669dbfc88cfdb1ab971
vanamelnik <jmvmiller@gmail.com>
2022-07-06 23:38:36 +0300

    Completed task19

--- task20/main.go
+++ task20/main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Задание 20
-// Разработать программу, которая переворачивает слова в строке. 
+// Разработать программу, которая переворачивает слова в строке.
 // Пример: «snow dog sun — sun dog snow».
 
+// reverseWords меняет порядок слов в строке на противоположный.
+func reverseWords(s string) string {
+	words := strings.Fields(s)
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 { // действуем с двух сторон))
+		words[i], words[j] = words[j], words[i]
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	s := "катацумури соро соро ноборэ фудзи но яма"
+	fmt.Printf("%q =====> %q\n", s, reverseWords(s))
+}
