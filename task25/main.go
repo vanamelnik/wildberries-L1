c758fedd4216de56767ab37bde076d2129191c60
vanamelnik <jmvmiller@gmail.com>
2022-07-07 13:32:58 +0300

    Completed task25

--- task25/main.go
+++ task25/main.go
@@ -3,3 +3,31 @@ package main
 // Задание 25
 // Реализовать собственную функцию sleep.
 
+import (
+	"fmt"
+	"time"
+)
+
+// sleepLoop запускает цикл, работающий до момента, когда текущее время совпадает с заданным
+// работает, но грузит процессор!
+func sleepLoop(interval time.Duration) {
+	wakeUp := time.Now().Add(interval)
+	for time.Now().Before(wakeUp) {
+	}
+}
+
+// sleepTimeAfter использует функцию time.After. Процессор не грузит.
+func sleepTimeAfter(interval time.Duration) {
+	<-time.After(interval)
+}
+
+func main() {
+	interval := time.Second * 3
+	fmt.Printf("sleepTimeAfter: Sleeping for %v...", interval)
+	sleepTimeAfter(interval)
+	fmt.Println(" OK")
+
+	fmt.Printf("sleepLoop: Sleeping for %v...", interval)
+	sleepLoop(interval)
+	fmt.Println(" OK")
+}
